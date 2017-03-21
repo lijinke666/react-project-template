26202aeca3263400b56cb5e1ba337c4e1418ec5d
jinke.li <jinke.li@feitian-tech.com>
2017-03-21 18:29:41 +0800

    feat:add react reducer update

--- src/app/components/Input/reducer.js
+++ src/app/components/Input/reducer.js
@@ -1,3 +1,5 @@
+import update from "react/lib/update"
+console.log(update)
 const initialState = {
   color: "rgb(0,0,0)", 
   size: "400" 
@@ -8,10 +10,7 @@ export default function reducer(state = initialState,action) {
     switch (action.type){
         case 'colorChange':
           // {...state,color}  ===>  Object.assign({},state,{color})
-          return {
-            ...state,
-            color
-          }
+          return update(state,{color})    //react 的一种更新方法
         case 'sizeChange':
           return {
             ...state,
